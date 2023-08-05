763fa4ea7bf243b66677d3726a0d73903a07638b
Gayan Kalinga Bulathsinhala <bg15407@gmail.com>
2023-08-05 22:38:45 +0530

    Added tab view & placeholder for home screen

--- FlashFitMobileApp/ContentView.swift
+++ FlashFitMobileApp/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            TabView{
+                HomeScreenView()
+                    .tabItem{
+                        Label("Home", systemImage: "house.fill")
+                    }
+                
+                
+                
+            }
+            .navigationTitle("Flash Fit")
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button{
+                        //NavigationLink("Help", destination: HistoryView())
+                    } label: {
+                        Image(systemName: "questionmark.circle.fill")
+                            .font(.system(.title2))
+                        
+                    }
+                }
+            
+            
         }
         .padding()
+        }
     }
 }
 
---
+++ FlashFitMobileApp/HomeScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreenView.swift
+//  FlashFitMobileApp
+//
+//  Created by Gayan Kalinga on 2023-08-05.
+//
+
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreenView()
+    }
+}
