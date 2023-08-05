773b622e74d36a46087889fc113fcac55f129b85
Gayan Kalinga Bulathsinhala <bg15407@gmail.com>
2023-08-05 22:21:19 +0530

    initial commit with the icon

---
+++ FlashFitMobileApp/FlashFitMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashFitMobileAppApp.swift
+//  FlashFitMobileApp
+//
+//  Created by Gayan Kalinga on 2023-08-05.
+//
+
+import SwiftUI
+
+@main
+struct FlashFitMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlashFitMobileAppUITests/FlashFitMobileAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FlashFitMobileAppUITestsLaunchTests.swift
+//  FlashFitMobileAppUITests
+//
+//  Created by Gayan Kalinga on 2023-08-05.
+//
+
+import XCTest
+
+final class FlashFitMobileAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
